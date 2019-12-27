54b799c3fca9114af5cd7b40977827aef168149b
eddsonjose <44173160+eddsonjose@users.noreply.github.com>
2019-12-26 17:27:32 -0800

    update

--- sketch.js
+++ sketch.js
@@ -1,9 +1,21 @@
 //game
 //eddson jose
+let player, playerX, playerY, playerSpeed;
 function setup() {
-  createCanvas(width, height);
-  background(100);
+  createCanvas(windowWidth, windowHeight);
+  playerX = width/2;
+  playerY = height/2;
+  playerSpeed = 5;
 }
 function draw() {
-  
+  fill(200); noStroke(); background(20);
+  circle(playerX, playerY, 5);
+  if (keyIsDown(87)) {playerY -= playerSpeed;}//w
+  if (keyIsDown(65)) {playerX -= playerSpeed;}//a
+  if (keyIsDown(83)) {playerY += playerSpeed;}//s
+  if (keyIsDown(68)) {playerX += playerSpeed;}//d
+  if (playerX <= 0) {playerX = 0;}
+  else if (playerX >= windowWidth) {playerX = windowWidth;}
+  else if (playerY <= 0) {playerY = 0;}
+  else if (playerY >= windowHeight) {playerY = windowHeight;}
 }
